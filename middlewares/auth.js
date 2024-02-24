560ab7ffc7211d4e00aa62203e1eb018709a30d2
groverdivyanshu <68288289+groverdivyanshu@users.noreply.github.com>
2024-02-24 17:55:02 +0530

    Update 1auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = async (req, res, next) => {
   try {
-    const { token } = req.cookies;
+    const  token = const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
     // console.log(token);
     if (!token) {
       return res.status(401).json({
